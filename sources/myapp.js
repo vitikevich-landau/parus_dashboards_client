9387384e1e0275aa90dfcdc2b0202b55fc01f9c9
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-04-21 17:07:39 +0700

    # Change start page start -> dashboards

--- sources/myapp.js
+++ sources/myapp.js
@@ -8,7 +8,7 @@ export default class MyApp extends JetApp{
 			version : VERSION,
 			router 	: BUILD_AS_MODULE ? EmptyRouter : HashRouter,
 			debug 	: !PRODUCTION,
-			start 	: "/top/start"
+			start 	: "/top/dashboards"
 		};
 
 		super({ ...defaults, ...config });
