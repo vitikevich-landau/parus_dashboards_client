d0dc9a712b274e3a7a78a2d664f92d8509846501
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-09-16 15:22:12 +0700

    # Отключены запросы к серверу при старте приложения

--- sources/models/enterCollection.js
+++ sources/models/enterCollection.js
@@ -1,6 +1,5 @@
-import {loadData, proxyWithThrottle, URL_TIMESHEET_ARRIVAL} from "./config";
+import {proxyWithThrottle, URL_TIMESHEET_ARRIVAL} from "./config";
 
 export const enterCollection = new webix.DataCollection({
-	url: loadData(URL_TIMESHEET_ARRIVAL),
 	save: proxyWithThrottle(URL_TIMESHEET_ARRIVAL),
 });
--- sources/models/exitCollection.js
+++ sources/models/exitCollection.js
@@ -1,6 +1,5 @@
-import {loadData, proxyWithThrottle, URL_TIMESHEET_LEAVE} from "./config";
+import {proxyWithThrottle, URL_TIMESHEET_LEAVE} from "./config";
 
 export const exitCollection = new webix.DataCollection({
-	url: loadData(URL_TIMESHEET_LEAVE),
 	save: proxyWithThrottle(URL_TIMESHEET_LEAVE),
 });
